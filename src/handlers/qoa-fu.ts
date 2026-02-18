f4b950be6e91d2e4dae6ccc1c6abdfff253c04b8
SladkyCitron <57183251+SladkyCitron@users.noreply.github.com>
2026-02-18 07:51:10 +0100

    Make tsc use JS qoa-fu instead of TS

--- src/handlers/qoa-fu.ts
+++ src/handlers/qoa-fu.ts
@@ -1,6 +1,6 @@
 import type { FileData, FileFormat, FormatHandler } from "../FormatHandler.ts";
 
-import { QOAEncoder, QOADecoder, QOABase } from "./qoa-fu/transpiled/QOA.ts";
+import { QOAEncoder, QOADecoder, QOABase } from "qoa-fu";
 import { WaveFile } from "wavefile";
 
 class uint8ArrayQOADecoder extends QOADecoder {
